728b30323137496605c49f3d0d2a9b8dda130264
larrygf02 <raulgf_02@hotmail.com>
2020-04-07 21:53:37 -0500

    React native start stylesheet

--- App.js
+++ App.js
@@ -1,19 +1,23 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react'
+import { View, Text, StyleSheet } from 'react-native'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+class HelloWorld extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Hola Mundo</Text>
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
-  },
-});
+    alignItems: 'center',
+    backgroundColor: 'blue'
+  }
+})
+
+export default HelloWorld
