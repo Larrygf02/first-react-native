d694848e8f1c5186228a1580c3507fcf3bbc2237
larrygf02 <raulgf_02@hotmail.com>
2020-04-10 10:45:22 -0500

    Manejo de estado js

--- App.js
+++ App.js
@@ -1,41 +1,22 @@
 import React, { Component } from 'react'
-import { View, Text, StyleSheet, Image, Button, Alert, ImageBackground, TouchableOpacity, TextInput } from 'react-native'
-import { WebView } from 'react-native-webview';
-
+import { View, Text, StyleSheet } from 'react-native'
+ 
 class HelloWorld extends Component {
-  saludo = () => {
-    Alert.alert('Este es una alerata')
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'Grace',
+      user: 'peque'
+    }
   }
   render() {
+    const { name, user } = this.state
     return (
-      <ImageBackground source={require('./assets/fondo-bin.jpg')} style={styles.container}>
-        <View style={styles.header}>
-          <View style={styles.headerLeft}>
-            <Image source={require('./assets/react.jpg')} style={styles.logo}></Image>
-          </View>
-          <View style={styles.headerRight}>
-            <Button title="Login" mode="android" onPress={this.saludo}></Button>
-          </View>
-        </View>
-        <View style={styles.body}>
-          <TouchableOpacity>
-            <Text style={styles.negrita}>Hola Mundo</Text>
-            <TextInput placeholder="Nombre de usuario" 
-            placeholderTextColor="white"
-            maxLength={8}
-            style={{ height: 40, borderColor: 'white', borderWidth: 1, padding: 1 }}></TextInput>
-            <View style={{ height: 300, width:300 }}>
-              <WebView
-                      style={ styles.WebViewContainer }
-                      javaScriptEnabled={true}
-                      domStorageEnabled={true}
-                      source={{uri: 'https://www.youtube.com/embed/O6Fd9fzPDPQ' }}
-              />
-            </View>
-          </TouchableOpacity>
-
-        </View>
-      </ImageBackground>
+      <View style={styles.container}>
+        <Text style={styles.title}>Manejo del Estado</Text>
+        <Text>Name: {name}</Text>
+        <Text>User: {user} </Text>
+      </View>
     )
   }
 }
@@ -43,37 +24,13 @@ class HelloWorld extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'column'
-  },
-  header: {
-    flex: 1,
-    flexDirection: 'row',
-    marginTop: 40
-  },
-  headerLeft: {
-    flex: 1,
-  },
-  headerRight: {
-    flex: 1,
-    marginTop:40
-  },
-  body: {
-    flex: 4,
+    justifyContent: 'center',
     alignItems: 'center'
   },
-  negrita: {
+  title: {
+    fontSize: 29,
     fontWeight: 'bold',
-    fontSize: 20,
-    color: 'white'
-  },
-  logo: {
-    width: 150,
-    height: 150,
-    borderRadius: 50,
-    resizeMode: 'contain'
-  },
-  WebViewContainer: {
-    marginTop: (Platform.OS == 'android') ? 20 : 0,
+    marginBottom: 10
   }
 })
 
---
+++ src/components/Intro.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react'
+import { View, Text, StyleSheet, Image, Button, Alert, ImageBackground, TouchableOpacity, TextInput } from 'react-native'
+import { WebView } from 'react-native-webview';
+
+class Intro extends Component {
+    saludo = () => {
+        Alert.alert('Este es una alerata')
+    }
+    render() {
+        return (
+            <ImageBackground source={require('./assets/fondo-bin.jpg')} style={styles.container}>
+            <View style={styles.header}>
+              <View style={styles.headerLeft}>
+                <Image source={require('./assets/react.jpg')} style={styles.logo}></Image>
+              </View>
+              <View style={styles.headerRight}>
+                <Button title="Login" mode="android" onPress={this.saludo}></Button>
+              </View>
+            </View>
+            <View style={styles.body}>
+              <TouchableOpacity>
+                <Text style={styles.negrita}>Hola Mundo</Text>
+                <TextInput placeholder="Nombre de usuario" 
+                placeholderTextColor="white"
+                maxLength={8}
+                style={{ height: 40, borderColor: 'white', borderWidth: 1, padding: 1 }}></TextInput>
+                <View style={{ height: 300, width:300 }}>
+                  <WebView
+                          style={ styles.WebViewContainer }
+                          javaScriptEnabled={true}
+                          domStorageEnabled={true}
+                          source={{uri: 'https://www.youtube.com/embed/O6Fd9fzPDPQ' }}
+                  />
+                </View>
+              </TouchableOpacity>
+    
+            </View>
+          </ImageBackground>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      flexDirection: 'column'
+    },
+    header: {
+      flex: 1,
+      flexDirection: 'row',
+      marginTop: 40
+    },
+    headerLeft: {
+      flex: 1,
+    },
+    headerRight: {
+      flex: 1,
+      marginTop:40
+    },
+    body: {
+      flex: 4,
+      alignItems: 'center'
+    },
+    negrita: {
+      fontWeight: 'bold',
+      fontSize: 20,
+      color: 'white'
+    },
+    logo: {
+      width: 150,
+      height: 150,
+      borderRadius: 50,
+      resizeMode: 'contain'
+    },
+    WebViewContainer: {
+      marginTop: (Platform.OS == 'android') ? 20 : 0,
+    }
+  })
+
+export default Intro
