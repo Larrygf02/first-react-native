f1e36adf21d001356784d1e3c778f62237af0c71
larrygf02 <raulgf_02@hotmail.com>
2020-04-15 23:22:11 -0500

    Navigation in actions

--- src/components/review/home.js
+++ src/components/review/home.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native'
+import { StyleSheet, View, Text, Button } from 'react-native'
 import { globalStyles } from '../../../styles/global';
 
-function Home(){
+function Home({navigation}){
+    const pressHandler = () => {
+        navigation.navigate('ReviewDetails')
+        //navigation.push('ReviewDetails')
+    }
     return (
         <View style={globalStyles.container}>
             <Text style={globalStyles.titleText}>Home Screen</Text>
+            <Button title="Go to review" onPress={pressHandler}></Button>
         </View>
     )
 }
