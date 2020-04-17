c04d0beecbbe1f9ebf683a8bd4a0f7d41f621435
larrygf02 <raulgf_02@hotmail.com>
2020-04-17 00:39:33 -0500

    Start Changes

--- src/components/review/reviewDetails.js
+++ src/components/review/reviewDetails.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, View, Text, Button } from 'react-native'
 import { globalStyles } from '../../../styles/global';
 import Card from '../../../shared/card';
 import Icon from 'react-native-vector-icons/AntDesign';
 
 function ReviewDetails({navigation}){
+    const [icon, setIcon ] = useState('staro')
     const pressHandler = () => {
         navigation.navigate('Home')
         //navigation.push('ReviewDetails')
     }
     const setRating = () => {
         console.log('Hello')
+    }
+    const stars = [1,2,3,4,5]
+    const shouldPaintIcon = (num) => {
+        if (icon == 'staro') {
+            setIcon('star')
+            return 'star'
+        }else{
+            setIcon('staro')
+            return 'staro'
+        }
     }
     return (
         <View style={globalStyles.container}>
@@ -19,8 +30,12 @@ function ReviewDetails({navigation}){
                 <Text>{navigation.getParam('body')}</Text>
                 <Text>{navigation.getParam('rating')}</Text>
                 <View style={styles.ratings}>
-                    <Icon name="staro" size={25} onPress={setRating}/>
-                    <Icon name="staro" size={25} />
+                    {stars.map((star) => {
+                        console.log(star)
+                        return (
+                            <Icon name="staro" size={25} />
+                        )
+                    })}
                 </View>
             </Card>
             <Button title="Go to home" onPress={pressHandler}></Button>
