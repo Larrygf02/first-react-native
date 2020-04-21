c9039e04d93eab7f969fa3413297c748785fea6e
larrygf02 <raulgf_02@hotmail.com>
2020-04-20 23:51:07 -0500

    Animations scrollable

--- App.js
+++ App.js
@@ -21,6 +21,33 @@ class App extends Component {
       outputRange: [HEADER_MAX_HEIGHT, HEADER_MIN_HEIGHT],
       extrapolate: 'clamp'
     })
+
+    const profileImageHeight= this.state.scrollY.interpolate({
+      inputRange: [0, HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT],
+      outputRange: [PROFILE_IMAGE_MAX_HEIGHT, PROFILE_IMAGE_MIN_HEIGHT],
+      extrapolate: 'clamp'
+    })
+
+    const profileImageMarginTop= this.state.scrollY.interpolate({
+      inputRange: [0, HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT],
+      outputRange: [HEADER_MAX_HEIGHT - (PROFILE_IMAGE_MAX_HEIGHT/ 2), HEADER_MAX_HEIGHT + 5],
+      extrapolate: 'clamp'
+    })
+
+    const headerZindex= this.state.scrollY.interpolate({
+      inputRange: [0, HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT],
+      outputRange: [0,1],
+      extrapolate: 'clamp'
+    })
+
+    const headerTitleBottom = this.state.scrollY.interpolate({
+      inputRange: [0, HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT, 
+        HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT + 5 + PROFILE_IMAGE_MIN_HEIGHT,
+        HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT + 5 + PROFILE_IMAGE_MIN_HEIGHT + 26
+      ],
+      outputRange: [-20,-20, -20, 0],
+      extrapolate: 'clamp'
+    })
     return (
       <View styles={{ flex: 1 }}>
         <Animated.View style={{
@@ -29,14 +56,30 @@ class App extends Component {
           left: 0,
           right: 0,
           backgroundColor: 'lightskyblue',
-          height: headerHeight
-        }}></Animated.View>
+          height: headerHeight,
+          zIndex: headerZindex,
+          alignItems: 'center'
+        }}>
+            <Animated.View style={{position: 'absolute', bottom: headerTitleBottom}}>
+              <Text style={{fontSize: 14, fontWeight: 'bold', color: 'white'}}>Ernesto</Text>
+            </Animated.View>
+
+        </Animated.View>
         <ScrollView scrollEventThrottle={16} onScroll={Animated.event(
           [{nativeEvent: {contentOffset: {y: this.state.scrollY}}}]
         )}>
-          <View style={styles.image}>
+          <Animated.View style={{
+            height: profileImageHeight,
+            width: profileImageHeight,
+            borderRadius: PROFILE_IMAGE_MAX_HEIGHT/2,
+            borderColor: 'white',
+            borderWidth: 3,
+            overflow: 'hidden',
+            marginTop: profileImageMarginTop,
+            marginLeft: 10
+          }}>
             <Image source={require('./assets/face-man.jpg')} style={{ flex: 1, width: null, height: null}}></Image>
-          </View>
+          </Animated.View>
           <View>
             <Text style={styles.profile_text}>Ernesto</Text>
           </View>
