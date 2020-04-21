375bd9f2430c8f94dd35ee232a7475d49729e466
larrygf02 <raulgf_02@hotmail.com>
2020-04-20 23:22:54 -0500

    Animated Images

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, ScrollView, Image } from 'react-native'
+import { View, Text, StyleSheet, ScrollView, Image, Animated } from 'react-native'
 
 HEADER_MIN_HEIGHT = 70
 HEADER_MAX_HEIGHT = 120
@@ -7,17 +7,40 @@ PROFILE_IMAGE_MAX_HEIGHT = 80
 PROFILE_IMAGE_MIN_HEIGHT = 40
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      scrollY: new Animated.Value(0)
+    }
+  }
   render() {
+
+    const headerHeight= this.state.scrollY.interpolate({
+      inputRange: [0, HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT],
+      outputRange: [HEADER_MAX_HEIGHT, HEADER_MIN_HEIGHT],
+      extrapolate: 'clamp'
+    })
     return (
       <View styles={{ flex: 1 }}>
-        <View style={styles.header}></View>
-        <ScrollView>
+        <Animated.View style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          right: 0,
+          backgroundColor: 'lightskyblue',
+          height: headerHeight
+        }}></Animated.View>
+        <ScrollView scrollEventThrottle={16} onScroll={Animated.event(
+          [{nativeEvent: {contentOffset: {y: this.state.scrollY}}}]
+        )}>
           <View style={styles.image}>
             <Image source={require('./assets/face-man.jpg')} style={{ flex: 1, width: null, height: null}}></Image>
           </View>
           <View>
-            <Text style={styles.profile_text}>Raul</Text>
+            <Text style={styles.profile_text}>Ernesto</Text>
           </View>
+          <View style={{height: 10000}}></View>
         </ScrollView>
       </View>
     )
@@ -36,8 +59,7 @@ const styles = StyleSheet.create({
     top: 0,
     left: 0,
     right: 0,
-    backgroundColor: 'lightskyblue',
-    height: HEADER_MAX_HEIGHT
+    backgroundColor: 'lightskyblue'
   },
   image: {
     height: PROFILE_IMAGE_MAX_HEIGHT,
